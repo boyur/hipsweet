3aba750be618eaa7d815fbf031b50a6fb8771197
Boyur <gboyur@gmail.com>
2016-07-09 17:57:16 +0300

    Добавил слайдер (есть небольшой касяк) Создал 5 слайдов.

--- js/main.js
+++ js/main.js
@@ -1,8 +1,27 @@
 $(document).ready(function() {
+    
+    // Слайдер
+    var btn = 1;
 	$('.slider__dot').on('click', function(e){
-		console.log(e.target.id);
+        var activeBtn = e.target.id;
+        var idActiveBtn = +activeBtn.replace(/btn/, "");
+        var slider = "#slider" + idActiveBtn;
+
+        if (btn !== idActiveBtn) {
+            $("#btn" + idActiveBtn).addClass( "slider__dot--active" );
+            $("#btn" + btn).removeClass( "slider__dot--active" );
+
+            if (btn < idActiveBtn) {
+                $("#slider" + btn).animate({left: "-100%"}, 1500 );
+                $("#slider" + idActiveBtn).animate({left: "0%"}, 1500 );
+            } else {
+                $("#slider" + btn).animate({left: "100%"}, 1500 );
+                $("#slider" + idActiveBtn).animate({left: "0%"}, 1500 );
+            }
+
+            btn = idActiveBtn;
+        }
+
 	});
+
 });
-
-
-
