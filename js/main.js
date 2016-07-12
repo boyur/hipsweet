1f9b29e53794502c9704bb69caff1a17c09cf63b
Boyur <gboyur@gmail.com>
2016-07-12 11:08:15 +0300

    Редактирую JS слайер, не могу получить масив сладов чтобы отом их двигать.

--- js/main.js
+++ js/main.js
@@ -1,59 +1,74 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    // Слайдер
-    var btn = 1;
-	$('.slider__dot').on('click', function(e){
-        var activeBtn = e.target.id;
-        var idActiveBtn = +activeBtn.replace(/btn/, "");
-        var slider = "#slider" + idActiveBtn;
+        // Слайдер
+    (function () {
 
-        if (btn !== idActiveBtn) {
-            $("#btn" + idActiveBtn).addClass( "slider__dot--active" );
-            $("#btn" + btn).removeClass( "slider__dot--active" );
+        var sliderArr = new Array();
+        $('.bestseller__slider-main').each(function (i) {
+            sliderArr.push(this);
+        });
 
-            if (btn < idActiveBtn) {
-                $("#slider" + btn).animate({left: "-100%"}, 1500 );
-                $("#slider" + idActiveBtn).animate({left: "0%"}, 1500 );
-            } else {
-                $("#slider" + btn).animate({left: "100%"}, 1500 );
-                $("#slider" + idActiveBtn).animate({left: "0%"}, 1500 );
+        console.log(sliderArr);
+        var btn = 1;
+        $('.slider__dot').on('click', function (e) {
+            console.log(sliderArr[0]);
+            var activeBtn = e.target.id;
+            var idActiveBtn = +activeBtn.replace(/btn/, "");
+            var slider = "#slider" + idActiveBtn;
+            //var sliderArr = $('#bestsellerSlider').children();
+
+            if (btn !== idActiveBtn) {
+                $("#btn" + idActiveBtn).addClass("slider__dot--active");
+                $("#btn" + btn).removeClass("slider__dot--active");
+
+
+
+                if (btn < idActiveBtn) {
+                    sliderArr[btn].animate({left: "-100%"}, 1500);
+                    sliderArr[idActiveBtn].animate({left: "0%"}, 1500);
+                } else {
+                    sliderArr[btn].animate({left: "100%"}, 1500);
+                    sliderArr[idActiveBtn].animate({left: "0%"}, 1500);
+                }
+
+                btn = idActiveBtn;
             }
 
-            btn = idActiveBtn;
-        }
-
-	});
+        });
+    }());
 
     //Аккордеон
-    var flag = true;
-    $('.acco__item').on('click', function(e){
-        e.preventDefault();
-        var
-            $this = $(this),
-            item = $this.closest('.acco__item'),
-            currentContent = item.find('.acco__content'),
-            duration = 500;
+    (function () {
+        var flag = true;
+        $('.acco__item').on('click', function (e) {
+            e.preventDefault();
+            var
+                $this = $(this),
+                item = $this.closest('.acco__item'),
+                currentContent = item.find('.acco__content'),
+                duration = 500;
 
-        if (flag) {
-            flag = false;
-            if (!item.hasClass('acco__item--active')) {
-                item
-                    .addClass('acco__item--active')
-                    .siblings()
-                    .removeClass('acco__item--active')
-                    .find('.acco__content')
-                    .slideUp(duration);
-                currentContent.slideDown(duration, function () {
-                    flag = true
-                });
-            } else {
-                item.removeClass('acco__item--active');
-                currentContent.slideUp(function() {
-                    flag = true
-                });
+            if (flag) {
+                flag = false;
+                if (!item.hasClass('acco__item--active')) {
+                    item
+                        .addClass('acco__item--active')
+                        .siblings()
+                        .removeClass('acco__item--active')
+                        .find('.acco__content')
+                        .slideUp(duration);
+                    currentContent.slideDown(duration, function () {
+                        flag = true
+                    });
+                } else {
+                    item.removeClass('acco__item--active');
+                    currentContent.slideUp(function () {
+                        flag = true
+                    });
+                }
             }
-        }
-    });
+        });
+    }());
 
     //phone mask
     (function () {
@@ -61,8 +76,8 @@ $(document).ready(function() {
     })();
 
     // tabs
-    (function(){
-        $('.team-tabs__item').on('click', function(e){
+    (function () {
+        $('.team-tabs__item').on('click', function (e) {
             e.preventDefault();
 
             var
